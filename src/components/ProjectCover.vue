ed5f6de5a3e3a76ed9342ca5f75560678ed59049
Claire Sersun <claire.sersun@gmail.com>
2023-11-06 22:59:45 -0500

    create covers

--- src/components/ProjectCover.vue
+++ src/components/ProjectCover.vue
@@ -1,121 +1,70 @@
-<template>
-    <div class="cover">
-        <RouterLink :to=projectLink class="cover-contents">
-            <img :src=projectImg alt="Project Cover Image"/>
-            <h1>
-                {{ title }}
-            </h1>
-        </RouterLink>
-    </div>
-
-        
+<script setup>
+import ProjectsInfo from './ProjectsInfo.vue';
+import LocomotiveScroll from 'locomotive-scroll';
 
 
-    
-</template>
-
-<script>
-
-export default {
-
-
-props: {
-        title: String,
-        projectImg: String,
-        projectLink: String
-},
-
-
-}
+defineProps(['projects'])
 
 
 
-    
+
 </script>
 
-<style scoped>
+<template>
+    <div 
+    class="cover" 
+    width="${projects.id}00vw">
+        <div 
+        class="cover-contents" >
+            <ProjectsInfo
+            
+            v-for="project in projects"
+            :key="project.id"
+            :ref="project.id"
+          :title="project.title"
+          :projectImg="project.image"
+          :projectLink="project.link"
+          />
+        </div>
+    </div>
+</template>
 
+<style scoped>
 .cover {
     display: flex;
     flex-direction: row;
     white-space: nowrap;
-  overflow-x: auto;
-  overflow-y: hidden;
+    overflow-x: auto;
+    overflow-y: hidden;
     align-items: flex-start;
     justify-content: flex-start;
-    height: 400vh !important;
-    width: 100vw;
+    height: 100vh;
     background: var(--color-background);
     color: var(--color-text);
 }
 
 .cover-contents {
-    position: static;
-top: 0;
+    flex-direction: column;
+    position: sticky;
+    top: 0;
     text-align: center;
     display: flex;
     flex-direction: column;
+    flex-wrap: wrap;
     align-items: center;
     justify-content: center;
     height: 100vh;
     width: 100vw;
     background: var(--color-background);
     color: var(--color-text);
-
-}
-.item {
-    display: inline-block;
-    width: 100px;
-    height: 100%;
-    outline: 1px dotted gray;
-    padding: 1em;
-    box-sizing: border-box;
-}
-
-.paddle {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  width: 3em;
-}
-.left-paddle {
-    left: 0;
-}
-.right-paddle {
-    right: 0;
-}
-.hidden {
-    display: none;
-}
-
-.print {
-  margin: auto;
-  max-width: 500px;
-}
     
-span {
-    display: inline-block;
-    width: 100px;
 }
 
 img {
     width: auto;
-    height: 100vh;
+    height: 100%;
     object-fit: cover;
-    position: relative;
-    align-self: center;
-    justify-self: center;
 }
 
-h1 {
-    font-size: 2rem;
-    font-weight: 500;
-    margin-top: 1rem;
-    color: var(--color-heading);
-    z-index: 75;
-    top: 50%;
-    position: fixed;
-
-}
 </style>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,32 +3,21 @@ import ProjectCover from '../components/ProjectCover.vue'
 import { ref } from 'vue'
 
 const projects = ref([
-  { id: 1, title: 'VALLETO', image: '../assets/logo.svg', link: '/projects/valleto' },
-  { id: 2, title: 'Moving For Life', image: '../assets/logo.svg', link: '/projects/valleto' },
-  { id: 3, title: 'timeslot', image: '../assets/logo.svg', link: '/projects/valleto' }
+  { id: 1, title: 'VALLETO', image: 'src/assets/logo.svg', link: '/projects/valleto' },
+  { id: 2, title: 'Moving For Life', image: 'src/assets/logo.svg', link: '/projects/valleto' },
+  { id: 3, title: 'timeslot', image: 'src/assets/logo.svg', link: '/projects/valleto' }
 ])
 
-console.log(projects.value)
 
 </script>
 
 <template>
   <main>
-      <ProjectCover 
-      v-for="project in projects"
-      :key="project.id"
-      :title="project.title"
-      :projectImg="project.image"
-      :projectLink="project.link"/>
+    <ProjectCover :projects="projects"/>
     
   </main>
 </template>
 
 <style scoped>
 
-img {
-  width: auto;
-  height: 100%;
-  object-fit: cover;
-}
 </style>
