bfca91ad59610001bd4328334e4ffada935bdd9e
Claire Sersun <claire.sersun@gmail.com>
2023-12-01 23:06:04 -0500

    VALLETO Project

--- src/views/projects/Valleto.vue
+++ src/views/projects/Valleto.vue
@@ -4,22 +4,29 @@
       src="../../assets/logo.svg" 
       alt="Valleto Logo"
       class="cover-img">
-      <div class="title">
-      <h1>VALLETO</h1>
-      <h2>Designer  ☁︎  UI / UX / Development / Graphic Design  ☁︎  2021-2022</h2>
-    </div>
+      <h1 class="title">VALLETO</h1>
+      <h2 class="subtitle">Designer  ☁︎  UI / UX / Development / Graphic Design  ☁︎  2021-2022</h2>
     <div class="content">
-      <h3>Problem</h3>
+      <h3 class="section-title">Problem</h3>
       <p>The site required maintenance and updates to keep up with the evolution of the dance company as new initiative were added, new works created, and the home city of the company changed. The site had slow load times and bounced many users off the site. There was a need for a merchandise store and a way for people to register for online zoom classes. Lastly, user experience needed to be elevated as well.</p>
-      <h3>User Experience</h3>
+      <div class="img-container">
+        <img src="https://static.wixstatic.com/media/877ffb_834a115324ae4bad8cf557cb979bf4c5~mv2.png/v1/fill/w_720,h_404,al_c,lg_1,q_85,enc_auto/9%20(1).png" alt="9 (1).png" width="564" height="317" style="width: 564px; height: 317px; object-fit: cover; object-position: 50% 50%;" fetchpriority="high" class="align-img-one">
+        <img src="https://static.wixstatic.com/media/877ffb_32e0a219d4c946a7ba33222055308ec6~mv2.png/v1/fill/w_720,h_404,al_c,lg_1,q_85,enc_auto/8-transparent.png" alt="8-transparent.png" width="551" height="309" style="width: 551px; height: 309px; object-fit: cover; object-position: 50% 50%;" fetchpriority="high" class="align-img-two">
+      </div>
+      <h3 class="section-title">User Experience</h3>
       <p>When I first started working on the site, there were 8+ options in the menu. Menu items were congested which caused us to loose users. My goal was to simplify and streamline the menu bar while creating depth to the organization. There were 4 items that were related to the word 'Company' so I grouped them together to declutter the menu. This was by far the largest change, second to the workshops tab which has evolved over time to have between 2-4 nested pages. </p>
       <p>We also optimized the website to decrease load times by using CloudFare, changing many self hosted videos to Vimeo alternatives, and optimizing images.
       </p>
       <p>As the site evolved, constant upkeep and checks were made to ensure excellent mobile optimization. This is essential as a large group of our users view our site from Instagram on their mobile devices.</p>
-      <h3>Merchandise & Online Bookings</h3>
+      <div class="video-container">
+      <video preload="none" autoplay loop playsinline="true" src="https://video.wixstatic.com/video/877ffb_3578e80891eb487ebb622c790ae11914/1080p/mp4/file.mp4"></video>
+      <video preload="none" autoplay loop playsinline="true" src="https://video.wixstatic.com/video/877ffb_1a5f63c848e34a3b8eaeaa2784722ecf/1080p/mp4/file.mp4"></video>
+      </div>
+      <h3 class="section-title">Merchandise & Online Bookings</h3>
       <p>To satisfy the need for a merchandise store, I chose Shopify and integrated it into our preexisting website. This choice was due to the ease of linking print-on-demand services through the admin panel of Shopify. They also had a user friendly interface for admin upkeep and could integrate with our website including our given color palates. To encourage buyers, this page was placed in our main menu and given our demographic, MERCH was the chosen title.</p>
       <p>During COVID-19, the company turned to online dance classes, therefore a system for signing up, taking payment, and seeing who signed up for class had to be built. Because we use WordPress, we wanted a system that could seamlessly run straight from the backend. Then came Amelia. I integrated Amelia, setting up class informational pages, sign up forms and payment, log in pages for users and instructors, created instructor users in the backend, among others. </p>
-      <h3>Design</h3>
+      <img src="https://static.wixstatic.com/media/877ffb_8d47614aee934cbf87ab82ad1e6d41fb~mv2.png/v1/fill/w_1470,h_828,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/7.png" alt="7.png" width="735" height="414" style="width: 735px; height: 414px; object-fit: cover; object-position: 50% 50%;" fetchpriority="high" class="align-img-three">
+      <h3 class="section-title">Design</h3>
       <p>The hamburger menu required custom design elements. I used Illustrator to design each menu item, the regular and hovered text. Once I exported the PNG I uploaded it to WordPress and used custom CSS to layer in the images and the hover states.</p>
       <p>Adding new Work titles required several design elements to be made. First, the title of the page overlayed with images. This I did in both Illustrator and Photoshop. If no images were given, I would screenshot from videos provided. Next, the color theme had to be chosen. I often would use colors from the given video. Once the color palate was chosen, I would layer them into the background of the block elements. Once the page was created with videos, photos, and text provided, I would add it to the Works page. Once updating the project scroll, I would add custom CSS to overlay the image with text and create a hover state.</p>
       <p>Alongside creating the color palate for each Work, I created color palates for several other pages. Some palates were based off of other pages, others were images, and some were videos. I played in Photoshop and took inspiration from Miami, nature, and contemporary art.</p>
@@ -27,6 +34,19 @@
         As the company evolved, pages were redesigned to fit new needs and additional ones added. 
       </p>
       <p>I took performance photos of the Work MOON. See selections <a href="https://valletodance.com/project/moon">here.</a></p>
+      <video preload="none" autoplay loop playsinline="true" src="https://video.wixstatic.com/video/877ffb_c4aa71a825c34376ab753974259bcfc3/1080p/mp4/file.mp4"></video>
+      <div class="img-container">
+        <img src="https://static.wixstatic.com/media/877ffb_df97e8049bc943878f14dd23c6797b28~mv2.png/v1/fill/w_720,h_403,al_c,lg_1,q_85,enc_auto/6-transparent.png" alt="6-transparent.png" width="530" height="297" style="width: 530px; height: 297px; object-fit: cover; object-position: 50% 50%;" fetchpriority="high" class="align-img-four">
+        <img src="https://static.wixstatic.com/media/877ffb_798b1a78c91147f6b5dd2b18405d22a2~mv2.png/v1/crop/x_345,y_78,w_255,h_174/fill/w_357,h_241,al_c,lg_1,q_85,enc_auto/5-transparent.png" alt="5-transparent.png" width="227" height="153" style="width: 227px; height: 153px; object-fit: cover; object-position: 50% 50%;" fetchpriority="high" class="align-img-five">
+      </div>
+      <h3 class="section-title link">
+    <RouterLink 
+    to="/"
+    class="section-title link"
+    >
+    See More Projects
+  </RouterLink>
+  </h3>
     </div>
     </div>
   </template>
@@ -38,11 +58,12 @@
       display: flex;
       align-items: start;
       justify-content: center;
-      flex-direction: column;    }
+      flex-direction: column;    
+    }
     
     .cover-img {
       width: 100vw;
-      height: 45vh;
+      height: 85vh;
       object-fit: cover;
       position: relative;
       top: 0;
@@ -50,9 +71,23 @@
     }
 
     .title {
+      position: absolute;
+      align-self: center;
+      top: 40vh;
+      text-align: center;
+      font-size: 10rem;
+      font-family: 'Proza Libre', sans-serif;
+      font-weight: 500;
+      color: var(--color-heading);
+    }
+
+    .subtitle {
       position: relative;
       width: 60vw;
       align-self: center;
+      padding-top: 20px;
+      padding-bottom: 20px;
+      font-weight: 600;
     }
 
     .content {
@@ -61,35 +96,107 @@
       align-self: center;
     }
 
-h1 {
-  font-family: 'Proza Libre', sans-serif;
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  padding-top: 100px;
-  top: -10px;
-  text-align: left;
-}
+    .section-title {
+      font-family: 'Proza Libre', sans-serif;
+      font-size: 1.2rem;
+      text-align: left;
+      font-weight: 600;
+    }
 
-h3 {
-  font-family: 'Proza Libre', sans-serif;
-  font-size: 1.2rem;
-  text-align: left;
-}
+    h2 {
+      font-family: 'Proza Libre', sans-serif;
+      font-size: 1.2rem;
+      text-align: left;
+    }
 
-h2 {
-  font-family: 'Proza Libre', sans-serif;
-  font-size: 1.2rem;
-  text-align: left;
-}
+    p {
+      font-family: 'Proza Libre', sans-serif;
+        font-size: 1rem;
+        line-height: 1.5;
+        margin-bottom: 1rem;
+        text-align: left;
+    }
 
-p {
-  font-family: 'Proza Libre', sans-serif;
-    font-size: 1rem;
-    line-height: 1.5;
+    .img-container {
+      width: 100%;
+    }
+
+    .align-img-one {
+      position: relative;
+      margin: 0px 0px -133px calc((100% - 980px) * 0.5);
+      left: 391px;
+      grid-area: 6 / 1 / 7 / 2;
+      justify-self: start;
+      align-self: start;
+    }
+
+    .align-img-two {
+      position: relative;
+      margin: 0px 0px 10px calc((100% - 980px) * 0.5);
+      left: 14px;
+      grid-area: 7 / 1 / 8 / 2;
+      justify-self: start;
+      align-self: start;
+  }
+
+  .align-img-three {
+    position: relative;
+    margin: 0px 0px 10px calc((100% - 980px) * 0.5);
+    left: 122px;
+    grid-area: 16 / 1 / 17 / 2;
+    justify-self: start;
+    align-self: start;
+  }
+
+  .align-img-four {
+    position: relative;
+    margin: 0px 0px -142px calc((100% - 980px) * 0.5);
+    left: 224px;
+    grid-area: 22 / 1 / 23 / 2;
+    justify-self: start;
+    align-self: start;
+  }
+
+  .align-img-five {
+    position: relative;
+    /* height: 228px;
+    width: 0; */
+    /* left: 557px; */
+    /* top: 137px; */
+    margin: 0px 0px -180px calc((100% - 300px));
+    grid-area: 23 / 1 / 24 / 2;
+    justify-self: start;
+    align-self: start;
+    transform: rotate(270deg);
+  }
+
+  .video-container {
+    width: 100%;
+  }
+
+  video {
+    position: relative;
+    margin: 0px 0px 10px calc((100% - 980px) * 0.5);
+    left: 122px;
+      justify-self: start;
+      align-self: start;
     margin-bottom: 1rem;
-    text-align: left;
-}
+    margin-top: 1rem;
+    aspect-ratio: auto 744 / 397;
+    width: 744px;
+    height: 397px;
+  }
+
+  .section-title.link {
+    position: relative;
+    margin-top: 15rem;
+    margin-bottom: 6rem;
+    text-align: center;
+    font-size: 2rem;
+    font-family: 'Proza Libre', sans-serif;
+    font-weight: 600;
+    color: var(--color-heading);
+  }
   
   </style>
   
