879729e8fb6fdb5c3fc5a44a8f71bae6e4a91c53
Claire Sersun <claire.sersun@gmail.com>
2023-12-10 10:42:25 -0500

    modeling page

--- src/views/projects/ModelingView.vue
+++ src/views/projects/ModelingView.vue
@@ -10,46 +10,84 @@
     <iframe width="560" height="315" src="https://www.youtube.com/embed/X7glXyedyBE?si=wgJrYboIlQQlf7Do" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen autoplay loop playsinline="true" preload="none"></iframe>
       </div>
       <div class="img-container">
-        <img src="../../assets/modeling/headshot.jpg" alt="Headshot Image" class="align-img">
-        <img src="../../assets/modeling/MALVIE.png" alt="MALVIE Image" class="align-img">
-        <img src="../../assets/modeling/millie.jpeg" alt="millie Image" class="align-img">
-        <img src="../../assets/modeling/alexx.jpg" alt="alexx Image" class="align-img">
-        <img src="../../assets/modeling/alexx2.jpg" alt="alexx Image" class="align-img">
-        <img src="../../assets/modeling/claire.jpg" alt="claire Image" class="align-img">
-        <img src="../../assets/modeling/finaldist.jpg" alt="final distribution Image" class="align-img">
-        <img src="../../assets/modeling/claire2.jpg" alt="claire Image" class="align-img">
-        <img src="../../assets/modeling/quyndita.jpg" alt="quyndita Image" class="align-img">
-        <img src="../../assets/modeling/alexx3.jpg" alt="alexx Image" class="align-img">
-        <img src="../../assets/modeling/scott.jpg" alt="scott Image" class="align-img">
-        <img src="../../assets/modeling/nyc.jpg" alt="nyc Image" class="align-img">
-        <img src="../../assets/modeling/louisa.jpg" alt="louisa Image" class="align-img">
-        <img src="../../assets/modeling/roya.jpg" alt="roya Image" class="align-img">
-        <img src="../../assets/modeling/roya2.jpg" alt="roya Image" class="align-img">
-        <img src="../../assets/modeling/fashionweek.jpg" alt="Fashion Week Image" class="align-img">
-        <img src="../../assets/modeling/chains.jpg" alt="Chains Image" class="align-img">
-        <img src="../../assets/modeling/cover.jpeg" alt="Cover Image" class="align-img">
-        <img src="../../assets/modeling/fashionweek2.jpg" alt="Fashion Week Image" class="align-img">
-        <img src="../../assets/modeling/louisa2.jpg" alt="Louisa Image" class="align-img">
-        <img src="../../assets/modeling/william.jpg" alt="William Image" class="align-img">
-        <img src="../../assets/modeling/la.jpg" alt="LA Image" class="align-img">
-        <img src="../../assets/modeling/josephs.jpg" alt="Josephs Image" class="align-img">
-        <img src="../../assets/modeling/alexx4.jpg" alt="Alexx Image" class="align-img">
-        <img src="../../assets/modeling/bird.jpg" alt="Bird Image" class="align-img">
-        <img src="../../assets/modeling/jaqlin.jpg" alt="Jaqlin Image" class="align-img">
-        <img src="../../assets/modeling/daniel.jpg" alt="Daniel Image" class="align-img">
-        <img src="../../assets/modeling/ruby.jpg" alt="Ruby Image" class="align-img">
-        <img src="../../assets/modeling/la2.jpg" alt="LA Image" class="align-img">
-        <img src="../../assets/modeling/glitter.png" alt="Glitter Image" class="align-img">
-        <img src="../../assets/modeling/bird2.jpg" alt="bird Image" class="align-img">
-        <img src="../../assets/modeling/blue.jpg" alt="blue Image" class="align-img">
-        <img src="../../assets/modeling/beach.jpg" alt="beach Image" class="align-img">
-        <img src="../../assets/modeling/claire3.jpg" alt="Claire Image" class="align-img">
-        <img src="../../assets/modeling/natalie.jpg" alt="Natalie Image" class="align-img">
-        <img src="../../assets/modeling/cross.jpg" alt="cross Image" class="align-img">
-        <img src="../../assets/modeling/claire4.jpg" alt="Claire Image" class="align-img">
-        <img src="../../assets/modeling/glitter2.jpg" alt="glitter Image" class="align-img">
-        <img src="../../assets/modeling/longhair.jpg" alt="Long Hair Image" class="align-img">
-        <img src="../../assets/modeling/la3.jpg" alt="LA Image" class="align-img">
+        <img src="../../assets/modeling/headshot.jpg" alt="Headshot Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/MALVIE.png" alt="MALVIE Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/millie.jpeg" alt="millie Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/alexx.jpg" alt="alexx Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/alexx2.jpg" alt="alexx Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/claire.jpg" alt="claire Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/finaldist.jpg" alt="final distribution Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/claire2.jpg" alt="claire Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/quyndita.jpg" alt="quyndita Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/alexx3.jpg" alt="alexx Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/scott.jpg" alt="scott Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/nyc.jpg" alt="nyc Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/louisa.jpg" alt="louisa Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/roya.jpg" alt="roya Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/roya2.jpg" alt="roya Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/fashionweek.jpg" alt="Fashion Week Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/chains.jpg" alt="Chains Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/cover.jpeg" alt="Cover Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/fashionweek2.jpg" alt="Fashion Week Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/louisa2.jpg" alt="Louisa Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/william.jpg" alt="William Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/la.jpg" alt="LA Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/josephs.jpg" alt="Josephs Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/alexx4.jpg" alt="Alexx Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/bird.jpg" alt="Bird Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/jaqlin.jpg" alt="Jaqlin Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/daniel.jpg" alt="Daniel Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/ruby.jpg" alt="Ruby Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/la2.jpg" alt="LA Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/glitter.png" alt="Glitter Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/bird2.jpg" alt="bird Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/blue.jpg" alt="blue Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/beach.jpg" alt="beach Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/claire3.jpg" alt="Claire Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/natalie.jpg" alt="Natalie Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/cross.jpg" alt="cross Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/claire4.jpg" alt="Claire Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/glitter2.jpg" alt="glitter Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/longhair.jpg" alt="Long Hair Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/la3.jpg" alt="LA Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/ballet.jpg" alt="Ballet Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/alexx5.jpg" alt="Alexx Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/katelyn.jpg" alt="Katelyn Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/la4.jpg" alt="LA Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/arrojo.jpg" alt="Arrojo Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/nyc2.jpg" alt="NYC Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/nyc3.jpg" alt="NYC Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/glitter3.jpg" alt="Glitter Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/la5.jpg" alt="LA Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/kovasky.jpg" alt="Kovasky Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/louisa3.jpg" alt="Louisa Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/la6.jpg" alt="LA Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/louisa4.jpg" alt="Louisa Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/runway.jpg" alt="Runway Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/nyc4.jpg" alt="NYC Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/nyc5.jpg" alt="NYC Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/louisa5.jpg" alt="Louisa Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/alexx6.jpg" alt="Alexx Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/nyc6.jpg" alt="NYC Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/louisa6.jpg" alt="Louisa Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/jordan.jpg" alt="Jordan Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/natalie2.jpg" alt="Natalie Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/forrest.jpg" alt="Forrest Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/kovasky2.jpg" alt="Kovasky Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/ladoru.jpg" alt="Ladoru Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/katelyn2.jpg" alt="Katelyn Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/ladoru2.jpg" alt="Ladoru Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/natalie3.jpg" alt="Natalie Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/nyc7.jpg" alt="NYC Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/nyc8.jpg" alt="NYC Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/forrest2.jpg" alt="Forrest Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/louisa7.jpg" alt="Louisa Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/forrest3.jpg" alt="Forrest Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/forrest4.jpg" alt="Forrest Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/dance.png" alt="Dance Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/jaqlin2.jpg" alt="Jaqlin Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/nyc9.jpg" alt="NYC Image" loading="lazy" class="align-img">
+        <img src="../../assets/modeling/louisa8.jpg" alt="Louisa Image" loading="lazy" class="align-img">
       </div>
       <h3 class="section-title link">
     <RouterLink 
@@ -132,22 +170,22 @@
     }
 
     .modeling .img-container {
-      width: 100%;
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      justify-content: center;
-      margin-bottom: 2rem;
+    /* Prevent vertical gaps */
+    line-height: 0;
+    
+    -webkit-column-count: 5;
+    -webkit-column-gap:   0px;
+    -moz-column-count:    5;
+    -moz-column-gap:      0px;
+    column-count:         5;
+    column-gap:           0px; 
     }
 
     .modeling .align-img {
-      position: relative;
-      /* margin: 10px; */
-      height: auto;
-      width: 400px;
-      justify-self: start;
-      align-self: start;
-  }
+    /* Just in case there are inline attributes */
+    width: 100%;
+    height: auto;
+    }
 
   .modeling .video-container {
     width: 100%;
@@ -218,5 +256,34 @@
     font-weight: 600;
     color: var(--color-heading);
   }
+
+  @media (max-width: 1200px) {
+    .modeling .img-container {
+  -moz-column-count:    4;
+  -webkit-column-count: 4;
+  column-count:         4;
+  }
+}
+@media (max-width: 1000px) {
+    .modeling .img-container {
+  -moz-column-count:    3;
+  -webkit-column-count: 3;
+  column-count:         3;
+  }
+}
+@media (max-width: 800px) {
+    .modeling .img-container {
+  -moz-column-count:    2;
+  -webkit-column-count: 2;
+  column-count:         2;
+  }
+}
+@media (max-width: 400px) {
+    .modeling .img-container {
+  -moz-column-count:    1;
+  -webkit-column-count: 1;
+  column-count:         1;
+  }
+}
   
   </style>
